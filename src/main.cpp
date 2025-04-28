ce4b49d3db83dc80cc95286cbcc5efbe0905889a
PetrIgnatov <petrignatovefr@gmail.com>
2025-04-29 02:44:34 +0300

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <QApplication>
 #include "window.h"
+#include <vector>
+#include <cmath>
 
 bool isNum(std::string& s) {
 	int dots = 0;
@@ -75,245 +77,281 @@ int main(int argc, char** argv) {
 		out.open("/dev/stdout");
 	}
 	if (inpT) {
-		std::cout << "Select mode for program (1 for single equation, 2 for system of equations):\n";
+		std::cout << "Would you like to use one of existing functions? (Y/N)\n";
 	}
-	int mode = 0;
-	while (mode < 1 || mode > 2) {
+	inp >> inpS;
+	while (inpS != "N" && inpS != "n" && inpS != "Y" && inpS != "y") {
+		if (!inpT) {
+			return 0;
+		}
+		out << "Sorry! Looks like your input is wrong! Try again!\n";
 		inp >> inpS;
-		if (!isInt(inpS)) {
-			std::cout << "Unexpected value entered! Try again\n";
-			if (!inpT) { 
-				return 0;
-			}	
-			continue;
+	}
+	std::vector<double> x, y;
+	int n = 0;
+	double h = 0;
+	if (inpS == "N" || inpS == "n") {	
+		if (inpT) {
+			std::cout << "Enter n:\n";
 		}
-		mode = std::stoi(inpS);
-		if (mode < 1 || mode > 2) {
-			std::cout << "Unexpected value entered! Try again\n";
-			if (!inpT) { 
-				return 0;
+		while (n <= 2) {
+			inp >> inpS;
+			if (!isInt(inpS)) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) { 
+					return 0;
+				}	
+				continue;
 			}
+			n = std::stoi(inpS);
+			if (n <= 0) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) { 
+					return 0;
+				}
+			}
+		}
+		if (inpT) {
+			std::cout << "Now enter x and y for each point:\n";
+		}
+		x.resize(n);
+		y.resize(n);
+		for (int i = 0; i < n; ++i) {
+			inp >> inpS;
+			while (!isNum(inpS)) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) {
+					return 0;
+				}
+				inp >> inpS;
+			}
+			x[i] = std::stod(inpS);
+			if (i == 1) {
+				h = x[i]-x[i-1];
+			}
+			if (i > 1 && std::abs((x[i-1]-x[i]) - (x[i-2]-x[i-1])) > 0.0001) {
+				std::cout << "dx is not const! Try again!\n";
+				if (!inpT) {
+					return 0;	
+				}
+			}
+			inp >> inpS;
+			while (!isNum(inpS)) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) {
+					return 0;
+				}
+				inp >> inpS;
+			}
+			y[i] = std::stod(inpS);
 		}
 	}
-	switch (mode) {
-		case 1: {
-				int (*methods[3])(double, double, double, double &, GiNaC::ex &, GiNaC::symbol &, long long &);
-				methods[0] = binary;
-				methods[1] = simple_iter;
-				methods[2] = newton;
-				GiNaC::symbol x("x");
-				std::vector <GiNaC::ex> ex = {3*pow(x, 3)+1.7*pow(x, 2)-15.42*x+6.89, 2.74*pow(x, 3)-1.93*pow(x, 2)-15.28*x-3.72, pow(x, 3)+2.84*pow(x, 2)+5.606*x-14.766, sin(x)};
-				int ind = -1;
-				while (ind < 1 || ind > ex.size()) {
-					if (inpT) {
-						std::cout << "Select equation:\n";
-						for (size_t i = 0; i < ex.size(); ++i) {
-							std::cout << i+1 << ") " << ex[i] << "\n";
-						}
-					}
-					inp >> inpS;
-					if (!isInt(inpS)) {
-						std::cout << "Unexpected value entered! Try again\n";
-						if (!inpT) { 
-							return 0;
-						}
-						continue;
-					}
-					ind = std::stoi(inpS);
-					if (ind < 1 || ind > ex.size()) {
-						std::cout << "Unexpected value entered! Try again\n";
-						if (!inpT) { 
-							return 0;
-						}
-					}
-				}
-				ind--;
-				if (inpT) {
-					std::cout << "Select calculation method (1 for binary, 2 for simple iteration, 3 for Newton):\n";
-				}
-				int calcmode = 0;
-				while (calcmode < 1 || calcmode > 3) {
-					inp >> inpS;
-					if (!isInt(inpS)) {
-						std::cout << "Unexpected value entered! Try again\n";
-						if (!inpT) { 
-							return 0;
-						}
-						continue;
-					}
-					calcmode = std::stoi(inpS);
-					if (calcmode < 1 || calcmode > 3) {
-						std::cout << "Unexpected value entered! Try again\n";
-						if (!inpT) { 
-							return 0;
-						}
-					}
-				}
-				calcmode--;
-				if (inpT) {
-					std::cout << "Enter left and right border for interval:\n";
-				}
-				double l, r;
-				inpS = "a";
-				std::string inpS1 = "a";
-				while (!isNum(inpS) || !isNum(inpS1) || std::stod(inpS) >= std::stod(inpS1)) {
-					inp >> inpS >> inpS1;
-					if (!isNum(inpS) || !isNum(inpS1) || std::stod(inpS) >= std::stod(inpS1)) { 
-						std::cout << "Unexpected values! Try again\n";
-						if (!inpT) { 
-							return 0;
-						}
-					}
-				}
-				l = std::stod(inpS);
-				r = std::stod(inpS1);
-				double acc = -0.1;
-				if (inpT) {
-					std::cout << "Enter accuracy: \n";
-				}
-				while (acc < 0) {
-					inp >> inpS;
-					if (!isNum(inpS)) {
-						std::cout << "Unexpected accuracy value! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-						continue;
-					}
-					acc = std::stod(inpS);
-					if (acc < 0) {
-						std::cout << "Unexpected accuracy value! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-					}
-				}
-				double ans = 0;
-				long long iters = 0;
-				switch (methods[calcmode](l, r, acc, ans, ex[ind], x, iters)) {
-					case 0:
-						out << x << " = " << ans << "\nF(" << ans << ") = " << subst(ex[ind], x, ans) << "\nCalculating took " << iters << " iterations" << std::endl;
-						break;
-					case 1:
-						out << "Maximum iterations limit reached! The best possible value found is " << ans << std::endl;
-						break;
-					case 2:
-						out << "Unable to find roots! The process doesn't add up!" << std::endl;
-						break;
-				}
-				QApplication app(argc, argv);
-				window win(ex[ind], x, l, r, 0);
-				win.show();
-				return app.exec();
+	else {
+		if (inpT) {
+			std::cout << "Choose function (1 - sin(x)):\n";
+		}
+		while (n != 1) {
+			inp >> inpS;
+			if (!isInt(inpS)) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) { 
+					return 0;
+				}	
+				continue;
 			}
-		case 2: {
-				std::vector<double> a = {0, 0}, b = {1, 1}, res(2);
-				std::vector<GiNaC::symbol> xes = {GiNaC::symbol("x"), GiNaC::symbol("y")};
-				std::vector<std::vector<GiNaC::ex>> sys = {{0.1*pow(xes[0], 2)+xes[0]+0.2*pow(xes[1], 2)-0.3, 0.2*pow(xes[0], 2)+xes[1]+0.1*xes[0]*xes[1]-0.7}, {sin(xes[0]+1)-xes[1]-1.2, 2*xes[0]+cos(xes[1])-2}};
-				std::vector<std::vector<GiNaC::ex>> phisys = {{0.3-0.1*pow(xes[0],2)-0.2*pow(xes[1], 2), 0.7-0.2*pow(xes[0], 2)-0.1*xes[1]*xes[0]}, {(2-cos(xes[1]))/2, sin(xes[0]+1)-1.2}};
-				int ind = -1;
-				while (ind < 1 || ind > sys.size()) {
-					if (inpT) {
-						std::cout << "Select system (enter a single number):\n";
-						for (size_t i = 0; i < sys.size(); ++i) {
-							std::cout << i+1 << ") ";
-							for (size_t j = 0; j < sys[i].size(); ++j) {
-								std::cout << sys[i][j] << " = 0\n";
-							}
-						}
-					}
-					inp >> inpS;
-					if (!isInt(inpS)) {
-						std::cout << "Unexpected system value entered! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-						continue;
-					}
-					ind = std::stoi(inpS);
-					if (ind < 1 || ind > sys.size()) {
-						std::cout << "Unexpected system value entered! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-					}
+			n = std::stoi(inpS);
+			if (n <= 0) {
+				std::cout << "Unexpected value entered! Try again\n";
+				if (!inpT) { 
+					return 0;
 				}
-				ind--;
-				if (inpT) {
-					std::cout << "Enter left and right borders for each variable interval:\n";
-				}
-				for (size_t i = 0; i < xes.size(); ++i) {
-					if (inpT) {
-						std::cout << xes[i] << "\n";
-					}
-					inpS = "a";
-					std::string inpS1 = "a";
-					while (!isNum(inpS) || !isNum(inpS1) || std::stod(inpS) >= std::stod(inpS1)) {
-						inp >> inpS >> inpS1;
-						if (!isNum(inpS) || !isNum(inpS1) || std::stod(inpS) >= std::stod(inpS1)) { 
-							std::cout << "Unexpected border values! Try again\n";
-							if (!inpT) {
-								return 0;
-							}
-						}
-					}
-					a[i] = std::stod(inpS);
-					b[i] = std::stod(inpS1);
-				}
-				double acc = -0.1;
-				if (inpT) {
-					std::cout << "Enter accuracy: \n";
-				}
-				while (acc < 0) {
-					inp >> inpS;
-					if (!isNum(inpS)) {
-						std::cout << "Unexpected accuracy value! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-						continue;
-					}
-					acc = std::stod(inpS);
-					if (acc < 0) {
-						std::cout << "Unexpected accuracy value! Try again\n";
-						if (!inpT) {
-							return 0;
-						}
-					}
-				}
-				long long iters = 0;
-				std::vector<std::vector<double>> diffs;
-				switch (system_simple_iter(a, b, acc, res, phisys[ind], xes, iters, diffs)) {
-					case 0:
-						for (size_t i = 0; i < res.size(); ++i) {
-							out << xes[i] << " = " << res[i] << "\n";
-						}
-						out << "This took " << iters << " iterations\n";
-						for (size_t i = 0; i < diffs.size(); ++i) {
-							out << "Diffs " << i+1 << ":\n";
-							for (size_t j = 0; j < diffs[i].size(); ++j) {
-								out << "\t" << xes[j] << ": " << diffs[i][j] << "\n";
-							}
-						}
-						break;
-                                        case 1:
-                                                out << "Maximum iterations limit reached! The best possible values found are\n";
-						for (size_t i = 0; i < res.size(); ++i) {
-							out << xes[i] << " = " << res[i] << "\n";
-						}
-                                                break;
-                                        case 2:
-                                                out << "Unable to find roots! The process doesn't add up!\n";
-                                                break;
-
-				}
-				QApplication app(argc, argv);
-				window win(sys[ind], xes, a, b, 0.001, 0);
-				win.show();
-				return app.exec();
 			}
-		default:
-			out << "Unexpected mode value! Try again\n";
+		}
+		n = 5;
+		h = 0.5;
+		x = {0.5, 1.0, 1.5, 2.0, 2.5};
+		y.resize(5);
+		for (int i = 0; i < n; ++i) {
+			y[i] = sin(x[i]);
+			std::cout << y[i] << " ";
+		}
+	}
+	std::vector<std::vector<double>> dy(n, std::vector<double>(n, 0));
+	for (int i = 0; i < n; ++i) {
+		dy[i][0] = y[i];
+	}
+	for (int i = 1; i < n; ++i) {
+		for (int j = 0; j < n-i; ++j) {
+			dy[j][i] = dy[j+1][i-1]-dy[j][i-1];
+		}
+	}
+	std::vector <long long> fact(n);
+	std::vector <double> h_pow(n);
+	fact[0] = 1;
+	h_pow[0] = 1;
+	std::vector <double> a(n);
+	GiNaC::symbol xsymb("x");
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			out << dy[i][j] << " ";
+		}
+		out << "\n";
+	}
+	std::cout << "calculated dy" << std::endl;
+	double xval;
+	if (inpT) {
+		std::cout << "Enter x:\n";
+	}
+	inp >> xval;
+	GiNaC::ex newton = 0;
+{
+	GiNaC::ex t = (xsymb-x[0])/h;	
+	for (int i = 0; i < n; ++i) {
+		if (i > 0) {
+			fact[i] = fact[i-1]*i;
+			h_pow[i] = h_pow[i-1]*h;
+		}
+		GiNaC::ex coef = 1;
+		for (int j = 0; j < i; ++j) {
+			coef *= t-j;
+		}
+		newton += dy[0][i]*coef/fact[i];
+	}
+	out << "Newton says: " << newton.subs(xsymb == xval) << "\n";
+	std::cout << "Newton over" << std::endl;
+}
+{
+	GiNaC::ex ln = 0;
+	for (int i = 0; i < n; ++i) {
+		GiNaC::ex cur = 1;
+		for (int j = 0; j < n; ++j) {
+			if (i == j) {
+				continue;
+			}
+			cur *= (xsymb-x[j])/(x[i]-x[j]);
+		}
+		ln += y[i]*cur;
+	}
+	out << "Lagrange says: " << ln.subs(xsymb == xval) << "\n";
+	std::cout << "Lagrange over" << std::endl;
+}
+{
+	double gauss = 0;
+	double t = (xval-x[x.size()/2])/h;
+	int mid = x.size()/2;	
+	double coef = 1;
+	if (t >= 0) {
+		for (int i = 0; i <= n/2; ++i) {
+			if (mid-i < 0) {
+				break;
+			}
+			if (2*i >= fact.size()) {
+				fact.push_back(2*i*fact.back());
+			}
+			gauss += coef*dy[mid-i][2*i]/fact[2*i];
+			coef *= (t+i);
+			if (2*i+1 >= dy[mid-i].size()) {
+				break;
+			}
+			if (2*i+1 >= fact.size()) {
+				fact.push_back((2*i+1)*fact.back());
+			}
+			gauss += coef*dy[mid-i][2*i+1]/fact[2*i+1];
+			coef *= (t-i-1);
+		}
+	}
+	else {
+		for (int i = 0; i <= n/2; ++i) {
+			if (mid-i < 0) {
+				break;
+			}
+			if (2*i >= fact.size()) {
+				fact.push_back(2*i*fact.back());
+			}
+			gauss += coef*dy[mid-i][2*i]/fact[2*i];
+			coef *= (t-i);
+			if (2*i+1 >= dy[mid-i].size()) {
+				break;
+			}
+			if (2*i+1 >= fact.size()) {
+				fact.push_back((2*i+1)*fact.back());
+			}
+			gauss += coef*dy[mid-i-1][2*i+1]/fact[2*i+1];
+			coef *= (t+i+1);
+		}
+	}
+	out << "Gauss says: " << gauss << "\n";
+}
+	GiNaC::ex gauss = 0;
+{
+	GiNaC::ex t = (xsymb-x[x.size()/2])/h;
+	int mid = x.size()/2;	
+	GiNaC::ex coef = 1;
+	for (int i = 0; i <= n/2; ++i) {
+		if (mid-i < 0) {
 			break;
+		}
+		if (2*i >= fact.size()) {
+			fact.push_back(2*i*fact.back());
+		}
+		gauss += coef*dy[mid-i][2*i]/fact[2*i];
+		coef *= (t+i);
+		if (2*i+1 >= dy[mid-i].size()) {
+			break;
+		}
+		if (2*i+1 >= fact.size()) {
+			fact.push_back((2*i+1)*fact.back());
+		}
+		gauss += coef*dy[mid-i][2*i+1]/fact[2*i+1];
+		coef *= (t-i-1);
 	}
+}
+{	
+	int sz = x.size();
+	sz -= sz&1;
+	double t = (xval-x[sz/2])/h;
+	if (std::abs(t) <= 0.25) {
+		int mid = sz/2;
+		double coef1 = t;
+		double coef2 = t*t;
+		double steer = y[mid];
+		for (int i = 0; i <= n/2; ++i) {
+			if (mid-i-1 < 0 || 2*i+2 >= dy[mid-i-1].size()) {
+				break;
+			}
+			steer += coef1*(dy[mid-i-1][2*i+1]+dy[mid-i][2*i+1])/(fact[1+2*i]*2);
+			coef1 *= (t*t-(i+1)*(i+1));	
+			steer += coef2*dy[mid-i-1][2*i+2]/fact[2+2*i];
+			coef2 *= (t*t-(i+1)*(i+1));
+		}
+		out << "Steerling says " << steer << "\n";
+	}
+}
+{
+	int sz = x.size();
+	if (sz > 1) {
+		sz -= (1-sz&1);
+		double t = (xval-x[sz/2])/h;
+		if (std::abs(t) >= 0.25 && std::abs(t) <= 0.75) {
+			int mid = sz/2;
+			double coef2 = (t-0.5)*t*(t-1);
+			double coef1 = t*(t-1);
+			double bessel = (y[mid]+y[mid+1])/2+(t-0.5)*dy[mid][1];
+			for (int i = 0; i < n; ++i) {
+				if (mid-i-1 < 0 || 2*i+2 >= dy[mid-i-1].size()) {
+					break;
+				}
+				bessel += coef1*(dy[mid-i-1][2*i+2]+dy[mid-i][2*i+2])/(fact[2+2*i]*2);
+				coef1 *= (t+i+1)*(t-i-2);
+				bessel += coef2*dy[mid-i-1][2*i+3]/fact[3+2*i];
+				coef2 *= (t+i+1)*(t-i-2);
+			}
+			out << "Bessel says " << bessel << "\n";
+		}
+	}
+}
+	QApplication app(argc, argv);
+	window win(&newton, &gauss, &xsymb, x[0], x.back());
+	win.show();
+	return app.exec();
 }
--- src/window.cpp
+++ src/window.cpp
@@ -9,46 +9,16 @@
 #include <complex>
 #include <ginac/ginac.h>
 #include "methods.h"
+#include <algorithm>
 
-window::window(std::vector <GiNaC::ex>& funcs, std::vector<GiNaC::symbol>& s, std::vector<double>& a, std::vector<double>& b, double acc, QWidget* parent) : QWidget(parent) {
+window::window(GiNaC::ex* newtonfunc, GiNaC::ex* gaussfunc, GiNaC::symbol* xsymb, double a, double b, QWidget* parent) : QWidget(parent) {
+	newton = *newtonfunc;
+	gauss = *gaussfunc;
+	l = a;
+	r = b;
+	x = *xsymb;
 	this->setFixedHeight(800);
 	this->setFixedWidth(800);
-	r = 0;
-	for (size_t i = 0; i < a.size(); ++i) {
-		r = std::max(r, std::max(std::abs(a[i]), std::abs(b[i])));
-	}
-	l = -1*r;
-	coef = 700/(r-l);
-	for (double yval = a[1]; yval <= b[1]; yval += (b[1]-a[1])/1e3) {
-		for (double xval = a[0]; xval <= b[0]; xval += (b[0]-a[0])/1e3) {
-			GiNaC::exmap map;
-			map[s[0]] = xval;
-			map[s[1]] = yval;
-			for (size_t i = 0; i < funcs.size(); ++i) {
-				double sub = subst(funcs[i], map);
-				if (std::abs(sub) <= acc) {
-//					std::cout << xval << " " << yval << "\n";
-					arr.push_back(std::make_pair(400+xval*coef, 400-yval*coef));	
-				}
-			}
-		}
-	}
-}
-
-window::window(GiNaC::ex& func, GiNaC::symbol& s, double a, double b, QWidget* parent) : QWidget(parent) {
-	this->setFixedHeight(800);
-	this->setFixedWidth(800);
-	r = std::max(std::abs(a),std::abs(b));	
-	for (double xval = a; xval <= b; xval += (b-a)/1e5) {
-		double sub = subst(func, s, xval);
-		r = std::max(r, std::abs(sub));
-	}
-	l = -1*r;
-	coef = 700/(r-l);
-	for (double xval = a; xval <= b; xval += (b-a)/1e5) {
-		double sub = subst(func, s, xval);
-		arr.push_back(std::make_pair(400+xval*coef, 400-coef*sub));
-	}
 }
 
 void window::paintEvent(QPaintEvent* event) {
@@ -59,8 +29,16 @@ void window::paintEvent(QPaintEvent* event) {
 	painter.drawLine(410, 35, 400, 15);
 	painter.drawLine(765, 390, 785, 400);
 	painter.drawLine(765, 410, 785, 400);
-	double d = (r-l)/13;
+	double d = 2*(r+0.1)/13;
 	double lineCoef = 700/13;
+	double coef = 700/(2*(r+0.1));
+	double maxy = -1e9;
+	double delta = (r-l)/10.0;
+	std::cout << "Blue - Newton\nRed - Gauss\n";
+	for (double xval = l; xval <= r; xval += delta) {
+		maxy = std::max(maxy, std::max(GiNaC::ex_to<GiNaC::numeric>(gauss.subs(x == l)).to_double(), GiNaC::ex_to<GiNaC::numeric>(newton.subs(x == xval)).to_double()));
+	}
+	double coefy = 700/(2*(maxy+0.1));
 	for (int i = -6; i <= 6; ++i) {
 		painter.drawLine(390, i*lineCoef+400 ,410, i*lineCoef+400);
 		painter.drawLine(i*lineCoef+400, 390, i*lineCoef+400, 410);
@@ -68,10 +46,22 @@ void window::paintEvent(QPaintEvent* event) {
 		painter.drawText(370, 400-i*lineCoef, s);
 		if (i == 0) continue; 
 		painter.drawText(i*lineCoef+400, 420, s);
+	}	
+	QPen newtonpen(Qt::blue, 4);
+	painter.setPen(newtonpen);
+	QPointF prev(400+coef*l, 400-coefy*GiNaC::ex_to<GiNaC::numeric>(newton.subs(x == l)).to_double());	
+	for (double xval = l+delta; xval <= r; xval += delta) {
+		QPointF cur(400+coef*xval, 400-coefy*GiNaC::ex_to<GiNaC::numeric>(newton.subs(x == xval)).to_double());
+		painter.drawLine(prev, cur);
+		prev = cur;
 	}
-	for (int i = 1; i < arr.size(); ++i) {
-//		std::cout << arr[i].first << " " << arr[i].second << "\n";
-		painter.drawPoint(arr[i].first, arr[i].second);
+	QPen gausspen(Qt::red, 2);
+	painter.setPen(gausspen);
+	prev = QPointF(400+coef*l, 400-coefy*GiNaC::ex_to<GiNaC::numeric>(gauss.subs(x == l)).to_double());	
+	for (double xval = l+delta; xval <= r; xval += delta) {
+		QPointF cur(400+coef*xval, 400-coefy*GiNaC::ex_to<GiNaC::numeric>(gauss.subs(x == xval)).to_double());
+		painter.drawLine(prev, cur);
+		prev = cur;
 	}
 }
 
